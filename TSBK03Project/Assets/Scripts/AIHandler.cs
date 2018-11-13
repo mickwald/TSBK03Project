c0b8e92f72ad9ec4af4692cbc37e51979e2817de
michael <mickwald@hotmail.com>
2018-11-13 14:57:24 +0100

    Working ai movement from handler

--- TSBK03Project/Assets/Scripts/AIHandler.cs
+++ TSBK03Project/Assets/Scripts/AIHandler.cs
@@ -6,18 +6,28 @@ public class AIHandler : MonoBehaviour {
 
     public GameObject prefab;
     GameObject newchild;
+    int children = 0;
 
 	// Use this for initialization
 	void Start () {
         newchild = Instantiate(prefab,this.transform);
-        newchild.transform.rotation.Set(50, 50, 0,1);
+        Quaternion tmp = newchild.transform.rotation;
+        tmp.Set(50, 50, 0, 1);
+        newchild.transform.rotation = tmp;
+        this.transform.position.Set(5, 5, 5);
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        newchild.transform.position.Set(10+ 10 * Mathf.Sin(Time.time), 0, 0);
-        Debug.Log("child x set to " + (10 + Mathf.Sin(Time.time)));
+        Vector3 tmp = newchild.transform.position;
+        tmp.Set(10+ 10 * Mathf.Sin(Time.time), 0, 0);
+        newchild.transform.position = tmp;
+        Vector3 pos = this.transform.position;
+        pos.Set(5 + Mathf.Sin(Time.time), 0, 5 + Mathf.Cos(Time.time));
+        this.transform.position = pos;
+
+
 
     }
 
