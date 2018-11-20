1d3a4443fe6435b69676dcd572ad38068e6bab2d
mickwald <mickwald@hotmail.com>
2018-11-20 12:19:33 +0100

    Working crash and turn + VERY basic player movement, adding playerscript files

---
+++ TSBK03Project/assets/PlayerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+    
+    private float moveVertical;
+    private Rigidbody rb;
+    private float moveHorizontal;
+    public float playerSpeed = .1f;
+	// Use this for initialization
+	void Start () {
+        rb = this.GetComponent<Rigidbody>();
+        rb.maxAngularVelocity = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        moveVertical = Input.GetAxis("Vertical");
+        moveHorizontal = Input.GetAxis("Horizontal");
+        this.transform.Translate(moveHorizontal * playerSpeed, 0, moveVertical * playerSpeed, Space.World);
+	}
+    
+}
