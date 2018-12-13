6ad1db476ad7d8a33d214bf297b443d67f2d7c87
Ehmanx <ehmanx@gmail.com>
2018-12-13 11:49:54 +0100

    forgot to add files

---
+++ TSBK03Project/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+    
+    private float moveVertical;
+    private Rigidbody rb;
+    private float moveHorizontal;
+	private float moveStrafe;
+    public float playerSpeed = .1f;
+	// Use this for initialization
+	void Start () {
+        rb = this.GetComponent<Rigidbody>();
+        rb.maxAngularVelocity = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.rotation = Quaternion.Euler(0, this.transform.rotation.eulerAngles.y, 0);
+		this.transform.position = new Vector3(transform.position.x, 0.0f, transform.position.z);
+        moveVertical = Input.GetAxis("Vertical");
+        moveHorizontal = Input.GetAxis("Horizontal");
+		moveStrafe = Input.GetAxis ("Strafe");
+        this.transform.Translate(0, 0, moveVertical * playerSpeed, Space.Self);
+		this.transform.Rotate(360* moveHorizontal * Vector3.up * Time.deltaTime * playerSpeed);
+		this.rb.velocity = Vector3.zero;
+		this.transform.Translate (moveStrafe * playerSpeed, 0, 0, Space.Self);
+
+	}
+    
+}
