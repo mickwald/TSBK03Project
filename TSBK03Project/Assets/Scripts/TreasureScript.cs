8dc6f03877b41ea3bb4fa7bcf8b5c86b98cab429
Ehmanx <ehmanx@gmail.com>
2018-12-14 15:06:51 +0100

    added treasure prefab, treasureScript and goalscipt aswell as modifications in the scene

---
+++ TSBK03Project/Assets/Scripts/TreasureScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureScript : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate (new Vector3(15,30,45) * Time.deltaTime);
+		
+	}
+
+	public void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player") {
+			GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerScript> ().pickUpTreasure();
+			//pS.pickUpTreasure ();
+			this.gameObject.transform.parent.gameObject.SetActive(false);
+		}
+	}
+}
