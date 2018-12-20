dcb2358c00231f097f2815854771c866f288fa65
michael <mickwald@hotmail.com>
2018-12-20 11:09:14 +0100

    Fixed weird waypoint assignment behaviour

--- TSBK03Project/Assets/Scripts/AIHandler.cs
+++ TSBK03Project/Assets/Scripts/AIHandler.cs
@@ -30,7 +30,7 @@ public class AIHandler : MonoBehaviour {
             children++;
             for (int a = 0; a < waypoints.Length; a++)
             {
-                waypoints[a] = (a + 4 * ((i) % 4)) % wayPointList.transform.childCount;
+                waypoints[a] = (a + 4 * i) % wayPointList.transform.childCount;
                 Debug.Log(waypoints[a]);
             }
             newchild = Instantiate(prefab, this.transform);
